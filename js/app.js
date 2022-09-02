6830bc3149c35ce01c5c639c59e355675001f68c
MahadiMunna <mahadimunna.official@gmail.com>
2022-09-03 01:37:31 +0600

    working with display news

--- js/app.js
+++ js/app.js
@@ -15,3 +15,28 @@ const displayCategories = categories =>{
         allCategories.appendChild(categoryList)
     });
 }
+
+fetch('https://openapi.programming-hero.com/api/news/category/01')
+.then(res => res.json())
+.then(data => displayNews(data.data))
+
+const displayNews = allNews =>{
+    console.log(allNews)
+    const newsField = document.getElementById('news-container');
+
+    allNews.forEach = news =>{
+        console.log(news.category_id)
+        const newsDiv = document.createElement('div');
+        newsDiv.classList.add('col');
+        newsDiv.innerHTML = `
+        <div class="card">
+          <img src="..." class="card-img-top" alt="...">
+          <div class="card-body">
+            <h5 class="card-title">Card title</h5>
+            <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+          </div>
+        </div>
+        `;
+        newsField.appendChild(newsDiv);
+    }
+}
